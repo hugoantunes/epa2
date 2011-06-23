da8bf06c1637a4d97dedd8f2fd4a87fa177744d4
Hugo Seixas Antunes <hugo.santunes@gmail.com>
2011-06-23 17:29:28 -0300

    acertado admin

--- SimulaPass/settings.py
+++ SimulaPass/settings.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
+# Django settings for SimulaPass project.
 
 import os.path
-# Django settings for SimulaPass project.
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -50,41 +50,15 @@ USE_L10N = True
 # Example: "/home/media/media.lawrence.com/media/"
 MEDIA_ROOT = ''
 
-
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
 MEDIA_URL = ''
 
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
-
-# URL prefix for static files.
-# Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
-
 # URL prefix for admin static files -- CSS, JavaScript and images.
 # Make sure to use a trailing slash.
 # Examples: "http://foo.com/static/admin/", "/static/admin/".
 ADMIN_MEDIA_PREFIX = '/media/'
-
-# Additional locations of static files
-STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-)
-
-# List of finder classes that know how to find static files in
-# various locations.
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
-)
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = 'n&1wyptqpx9o3plhwbd5wz#uw6x9nalhuo@ay6yqo@=v(!xd7@'
@@ -118,14 +92,13 @@ TEMPLATE_DIRS = (
     # Don't forget to use absolute paths, not relative paths.
 	'templates'
 )
-
 INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+	'django.contrib.admin',
 	
 	'south',
 	'django_pytest',
