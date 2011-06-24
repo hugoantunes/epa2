2a4a927c9330569fa6691015ee7091c39dec9c90
Hugo Seixas Antunes <hugo.santunes@gmail.com>
2011-06-24 00:54:24 -0300

    teste unitario rodando mas ainda quebrado

---
+++ SimulaPass/passageiros/models.py
@@ -0,0 +1,4 @@
+from django.db import models
+
+class Passageiro(models.Model):
+	pass
---
+++ SimulaPass/passageiros/tests/test_unitario.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from unittest import TestCase
+from passageiros.models import Passageiro
+
+class TestUnitarioPassageiro(TestCase):
+	
+	def setUp(self):
+		pass
+	
+	def tearDown(self):
+		pass
+		
+	def test_passageiros_deveria_ter_atirbutos_corretos(self):
+		passageiro = Passageiro()
