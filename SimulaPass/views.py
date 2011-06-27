bf8190e7a448a2679fc322d206354f9fed02ee82
Hugo Seixas Antunes <hugo.santunes@gmail.com>
2011-06-26 22:04:08 -0300

    tentativa de serializar obj passageiro, ainda não funcionando

--- SimulaPass/views.py
+++ SimulaPass/views.py
@@ -1,11 +1,14 @@
 # encoding: utf-8
+import time
+
+from django.utils import simplejson
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
 
 from passageiros.models import Passageiro
 from transportes.models import Transporte
 
-import time
+
 
 def home(request):
     template = u'index.html'
@@ -13,10 +16,19 @@ def home(request):
     passageiros = Passageiro.objects.all()
     transportes = Transporte.objects.all()
     
+    json_passageiros = {'passageiros':[]}
+
+    for passageiro in passageiros:
+        json_passageiros['passageiros'].append({
+            'id':passageiro.id,
+            'nome':passageiro.nome,
+            'conforto_toleravel':passageiro.conforto_toleravel
+        })
+    
     context = {'passageiros':passageiros,
+            'json_passageiro':simplejson.dumps(json_passageiros),
             'transportes':transportes,
     }
-
 
     return render_to_response(template, context)
 
