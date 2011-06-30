c4845f8c159271bdaf659616841a6ba0686b0a4f
Hugo Seixas Antunes <hugo.santunes@gmail.com>
2011-06-30 03:20:29 -0300

    terminando ultimos ajustes do prototipo

--- SimulaPass/transportes/admin.py
+++ SimulaPass/transportes/admin.py
@@ -4,7 +4,7 @@ from transportes.models import Transporte
 from django.contrib import admin
 
 class TransporteAdmin(admin.ModelAdmin):
-	list_display = ('nome','tempo_viagem','coeficiente_conforto')
+	list_display = ('nome','tempo_viagem','coeficiente_conforto','capacidade_maxima')
 	list_filter = ['coeficiente_conforto']
 	search_fields = ['nome']
 	
