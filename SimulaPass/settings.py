a862f86e1d34b0e68a9a539b6d35f41e1531eb5d
Hugo Seixas Antunes <hugo.santunes@gmail.com>
2011-09-18 18:00:30 -0300

    alteracao no settings

--- SimulaPass/settings.py
+++ SimulaPass/settings.py
@@ -18,7 +18,7 @@ DATABASES = {
       'ENGINE': 'django.db.backends.mysql',       # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
       'NAME': 'simulapass',                       # Or path to database file if using sqlite3.
       'USER': 'root',                             # Not used with sqlite3.
-      'PASSWORD': '1234',                         # Not used with sqlite3.
+      'PASSWORD': '',                         # Not used with sqlite3.
       'HOST': '127.0.0.1',                        # Set to empty string for localhost. Not used with sqlite3.
       'PORT': '3306',                             # Set to empty string for default. Not used with sqlite3.
     }
@@ -101,6 +101,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
 	'django.contrib.admin',
+    'django_websocket', 
 	
 	'south',
 	'passageiros',
