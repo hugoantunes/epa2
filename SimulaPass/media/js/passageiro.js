b45102044bbd94ed4ef694241436919637a811d7
Hugo Seixas Antunes <hugo.santunes@gmail.com>
2011-10-02 21:50:59 -0300

    refactor do js que movimenta transporte

--- SimulaPass/media/js/passageiro.js
+++ SimulaPass/media/js/passageiro.js
@@ -1,201 +1,75 @@
 //MOVIMENTOS Q1
+var tops = [0, 15, 290]
+var rights = [0, 415, 838]
 
+function movimenta($obj, top, right){
+    if(right==0)
+        $obj.animate({top:top});
+    else if(top==0)
+        $obj.animate({right:right});
+    else
+        $obj.animate({top:top,right:right});
+}
+//MOVIMENTOS DE Q1
 function q1q3($obj){
-    primeiro_q1q3($obj);
-    segundo_q1q3($obj);
-    terceiro_q1q3($obj);
+    movimenta($obj, tops[1], rights[1]);
+    movimenta($obj, tops[2], rights[1]);
+    movimenta($obj, tops[2], rights[2]);
 }
 
 function q1q2($obj){
-    primeiro_q1q2($obj);
+    movimenta($obj, tops[0], rights[2]);
 }
 
 function q1q4($obj){
-    primeiro_q1q4($obj);
-}
-
-function primeiro_q1q4($obj){
-    $obj.animate({
-        top:280,
-    });
-}
-
-function primeiro_q1q2($obj){
-    $obj.animate({
-        right:840,
-    });
-}
-
-function primeiro_q1q3($obj){
-    $obj.animate({
-        right:415,
-        top:15,
-        duration:5
-    });
-}
-
-function segundo_q1q3($obj){           
-    $obj.animate({
-        right:415,
-        top:290
-    });
-}
-
-function terceiro_q1q3($obj){           
-    $obj.animate({
-        right:840,
-        top:290
-    });
+    movimenta($obj, tops[2], rights[0]);
 }
 //FIM MOVIMENTOS DE Q1
 
 //MOVIMENTOS DE Q2
 function q2q4($obj){
-    primeiro_q2q4($obj);
-    segundo_q2q4($obj);
-    terceiro_q2q4($obj);
+    movimenta($obj, tops[1], rights[2]);
+    movimenta($obj, tops[2], rights[2]);
+    movimenta($obj, tops[1], rights[2]);
 }
 
 function q2q1($obj){
-    primeiro_q2q1($obj);
+    movimenta($obj, tops[0], rights[1]);
 }
 
 function q2q3($obj){
-    primeiro_q2q3($obj);
+    movimenta($obj, tops[2], rights[0]);
 }
-
-function primeiro_q2q3($obj){
-    $obj.animate({
-        top:280,
-    });
-}
-
-function primeiro_q2q1($obj){
-    $obj.animate({
-        right:15,
-    });
-}
-
-function primeiro_q2q4($obj){
-    $obj.animate({
-        right:415,
-        top:15,
-        duration:5
-    });
-}
-
-function segundo_q2q4($obj){           
-    $obj.animate({
-        right:415,
-        top:290
-    });
-}
-
-function terceiro_q2q4($obj){           
-    $obj.animate({
-        right:15,
-        top:290
-    });
-}
-
 //FIM MOVIMENTOS Q2
 
 //MOVIMENTOS Q3
 function q3q1($obj){
-    primeiro_q3q1($obj);
-    segundo_q3q1($obj);
-    terceiro_q3q1($obj);
+    movimenta($obj, tops[2], rights[2]);
+    movimenta($obj, tops[1], rights[2]);
+    movimenta($obj, tops[1], rights[1]);
 }
 
 function q3q2($obj){
-    primeiro_q3q2($obj);
+    movimenta($obj, tops[1], rights[0]);
 }
 
 function q3q4($obj){
-    primeiro_q3q4($obj);
-}
-
-function primeiro_q3q2($obj){
-    $obj.animate({
-        top:15,
-    });
-}
-
-function primeiro_q3q4($obj){
-    $obj.animate({
-        right:15,
-    });
-}
-
-function primeiro_q3q1($obj){
-    $obj.animate({
-        right:415,
-        top:290,
-        duration:5
-    });
-}
-
-function segundo_q3q1($obj){           
-    $obj.animate({
-        right:415,
-        top:15
-    });
-}
-
-function terceiro_q3q1($obj){           
-    $obj.animate({
-        right:15,
-        top:15
-    });
+    movimenta($obj, tops[0], rights[1]);
 }
 //FIM MOVIMENTOS Q3
 //MOVIMENTOS Q4
 function q4q2($obj){
-    primeiro_q4q2($obj);
-    segundo_q4q2($obj);
-    terceiro_q4q2($obj);
+    movimenta($obj, tops[2], rights[1]);
+    movimenta($obj, tops[1], rights[1]);
+    movimenta($obj, tops[1], rights[2]);
 }
 
 function q4q1($obj){
-    primeiro_q4q1($obj);
+    movimenta($obj, tops[1], rights[0]);
 }
 
 function q4q3($obj){
-    primeiro_q4q3($obj);
-}
-
-function primeiro_q4q1($obj){
-    $obj.animate({
-        top:15,
-    });
-}
-
-function primeiro_q4q3($obj){
-    $obj.animate({
-        right:840,
-    });
-}
-
-function primeiro_q4q2($obj){
-    $obj.animate({
-        right:415,
-        top:290,
-        duration:5
-    });
-}
-
-function segundo_q4q2($obj){           
-    $obj.animate({
-        right:415,
-        top:15
-    });
-}
-
-function terceiro_q4q2($obj){           
-    $obj.animate({
-        right:840,
-        top:15
-    });
+    movimenta($obj, tops[0], rights[2]);
 }
 //FIM MOVIMENTOS Q4
 
