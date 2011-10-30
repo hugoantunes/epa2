3c1492f3a8b9ad6e6d0f1880903d7b48fa3c7aed
Hugo Seixas Antunes <hugo.santunes@gmail.com>
2011-10-30 10:23:40 -0200

    ajustes na view e no template

--- SimulaPass/views.py
+++ SimulaPass/views.py
@@ -46,6 +46,7 @@ def teste(request):
 def entra_no_transporte(request,id_mundo, id_quadrante):
     json={'passageiros':[], 'transportes': []}
     
+    
     mundo = Mundo.objects.get(id=id_mundo)
     simulacao = Simulacao.objects.filter(mundo=mundo)
     origem = mundo.quadrantes.get(id=id_quadrante)
@@ -64,9 +65,9 @@ def entra_no_transporte(request,id_mundo, id_quadrante):
             )
     else:
         simulacao = simulacao[0]
-
+    
     passageiro = AgentePassageiro.objects.create(
-            tipo_passageiro=tipos_passageiros[tipo_passageiro_escolhido],
+            tipo_passageiro=tipo_passageiro_escolhido,
             conforto_atual=100,
             simulacao=simulacao,
             origem=origem,
@@ -77,45 +78,85 @@ def entra_no_transporte(request,id_mundo, id_quadrante):
     if request.POST: 
         if mundo.qtd_transportes > simulacao.qtd_transportes_usados: 
 
-            transportes = Transporte.objects.all()            
+            tipos_transportes = Transporte.objects.all()            
             lista_transportes = [] 
 
-            for transporte in transportes:
-                lista_transportes.append(transporte)
+            for tipo_transporte in tipos_transportes:
+                lista_transportes.append(tipo_transporte)
             
             if mundo.qtd_carros >= simulacao.qtd_carros_usados:
                 lista_transportes.append('carro')
 
-            import ipdb; ipdb.set_trace()
+            #import ipdb; ipdb.set_trace()
+            #escolhe o transporte
             escolha = random.choice(lista_transportes)
             escolha = 0            
             if lista_transportes[escolha] == 'carro':
                 pass
             else:
-                tipo_transporte_escolhido = transportes[escolha]
-                
-                if simulacao.transportes:
-                    transportes_escolhidos = simulacao.transportes.filter(tipo_transporte=tipo_transporte_escolhido, capacidade_atual__lte=tipo_transporte_escolhido.capacidade_maxima, desconforto__lte=passageiro.tipo_passageiro.conforto_toleravel)
+                #quando não é carro
+                tipo_transporte_escolhido = tipos_transportes[escolha]
+               
+                if simulacao.transportes.all() and not simulacao.transportes.filter(tipo_transporte=tipo_transporte_escolhido) :
+                    #busca um transporte escolhido, que de pra ele entrar e confortavel.
+                    transportes_escolhidos = simulacao.transportes.filter(tipo_transporte=tipo_transporte_escolhido, capacidade_atual__lt=tipo_transporte_escolhido.capacidade_maxima, desconforto__lte=passageiro.tipo_passageiro.conforto_toleravel)
                     
                     qtd_transportes_escolhidos = len(transportes_escolhidos)
-                    
+                   
+                    #se existir mais de um ele escolhe um deles.
                     if qtd_transportes_escolhidos > 0:
-                        transporte = random.choice(transportes_escolhidos)
+                        tipo_transporte_escolhido = random.choice(transportes_escolhidos)
+                    else:
+                        #busca transporte escolhido mesmo desconfortavel
+                        transportes_escolhidos = simulacao.transportes.filter(tipo_transporte=tipo_transporte_escolhido, capacidade_atual__lt=tipo_transporte_escolhido.capacidade_maxima)
+                        qtd_transportes_escolhidos = len(transportes_escolhidos)
+                        if qtd_transportes_escolhidos > 0:
+                            tipo_transporte_escolhido = random.choice(transportes_escolhidos)     
+                        else:
+                            #busca por qualquer transporte que ele possa entrar
+                            transportes_escolhidos = simulacao.transportes.filter(capacidade_atual__lt=tipo_transporte_escolhido.capacidade_maxima).exclude(tipo_transporte=tipo_transporte_escolhido)
+                            #
+                            #
+                            #
+                            #ainda falta coisa aqui !!!
+                            #
+                            #
+                            #
+                        transporte = AgenteTransporte.objects.create(
+                           tipo_transporte = tipo_transporte_escolhido,
+                           simulacao=simulacao,
+                           origem=origem, 
+                           destino=destino,  
+                           capcidade_atual=0,
+                           desconforto=0,
+                           )
+
+                        transporte.capacidade_atual += 1
                 else:
-                    transporte = AgenteTransporte.objects.create(
-                            tipo_transporte = tipo_transporte_escolhido,
-                            simulacao=simulacao,
-                            origem=origem, 
-                            destino=destino,  
-                            capcidade_atual=0,
-                            desconforto=0,
-                            )
-                    
-                    passageiro.transporte = transporte
-                    passageiro.save()
+                    if mundo.qtd_transportes >= simulacao.qtd_transportes_usados+1:
 
-                    
+                        #se não existe nenhum transporte ainda desse tipo, ele é criado e o passageiro entra
+                        transporte = AgenteTransporte.objects.create(
+                                tipo_transporte = tipo_transporte_escolhido,
+                                simulacao=simulacao,
+                                origem=origem,
+                                destino=destino,
+                                capacidade_atual=0,desconforto=0,
+                                )
 
+                        transporte.capacidade_atual += 1
+
+            passageiro.transporte = transporte
+                       
+            if transporte.tipo_transporte.capacidade_confortavel < transporte.capacidade_atual:
+                desconforto = int(float(transporte.capacidade_confortavel)/float(transporte.transporte_tipo.capacidade_confortavel)*100) 
+                transporte.desconforto = desconforto
+            
+            transporte.save()
+            passageiro.save()
+            import ipdb; ipdb.set_trace()
+            
+            json['transportes'].append(transporte);
             json = simplejson.dumps(json)
         
     return HttpResponse(json, mimetype = 'application/json')
